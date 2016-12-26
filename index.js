19ffb28ba284c7fcb4370c2ebf8b2a23e3f818d5
Roman Paradeev <grumpy@yandex-team.ru>
2016-12-27 02:00:45 +0500

    move reducers to separate file

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-const chalk = require('chalk');
 const {oauth} = require('./config');
 const Metrica = require('./lib/metrica');
-const {zipObj} = require('./lib/utils');
+const {normalizeReport, extractBrowsers, extractVersions, extractOutput} = require('./lib/reducers');
 
 const metrica = new Metrica({oauth});
 
@@ -11,75 +10,14 @@ const threshold = process.argv[3];
 metrica.getCounters()
 	.then(xs => xs.find(x => x.site === site).id)
 	.then(id => metrica.getReport(id))
-	.then(normalizeReport)
 	.then(report => {
-		const browsers = report.reduce((acc, record) => {
-			const {browser, version, visits} = record;
-			const [major, minor] = version.split('.');
-
-			acc[browser] = acc[browser] || {};
-			acc[browser][major] = acc[browser][major] || {};
-			acc[browser][major][minor || ''] = visits;
-
-			return acc;
-		}, {});
-
-		Object.keys(browsers).forEach(browser => {
-			const majors = browsers[browser];
-			const versions = [];
-
-			Object.keys(majors).forEach(major => {
-				const minors = majors[major];
-
-				if (Object.keys(minors).length === 1 && '' in minors) {
-					versions.push({version: major, visits: minors['']});
-				} else {
-					Object.keys(minors).filter(x => x !== '').forEach(minor => {
-						versions.push({version: `${major}.${minor}`, visits: minors[minor]});
-					});
-				}
-			});
-
-			browsers[browser] = versions;
-		});
-
-		const output = Object.keys(browsers).map(browser => {
-			const versions = browsers[browser];
-
-			return `${browser} ` + versions.map(({version, visits}) => {
-				const rate = (visits * 100).toFixed(2);
-				const color = visits > threshold * 2 ? chalk.red : chalk.yellow;
-
-				return `${color(version)} ${chalk.dim(rate + '%')}`;
-			}).join(', ');
-		}).sort();
+		const normalized = normalizeReport(report, threshold);
+		const browsers = extractBrowsers(normalized);
+		const versions = extractVersions(browsers);
+		const output = extractOutput(versions, threshold);
 
 		output.forEach(line => console.log(line));
 	})
 	.catch(err => {
-		console.log(err.response.body);
+		console.log(err);
 	});
-
-function normalizeReport(report) {
-	const visits = zipObj(report.query.metrics, report.totals)['ym:s:visits'];
-
-	const records = report.data.map(record => ({
-		segments: zipObj(report.query.dimensions, record.dimensions),
-		visits: zipObj(report.query.metrics, record.metrics)['ym:s:visits']
-	}));
-
-	const browsers = records.map(version => ({
-		browser: version.segments['ym:s:browser'].name,
-		version: (
-			version.segments['ym:s:browserAndVersion'].id ||
-			version.segments['ym:s:browserAndVersionMajor'].id
-		).split('.').slice(1).join('.'),
-		visits: version.visits / visits
-	}));
-
-	const relevantBrowsers = browsers.filter(version => {
-		return version.visits >= threshold;
-	});
-
-	return relevantBrowsers;
-}
